06edcc93a27c0ab6005c486e636083d050f99857
Szeghi Dorottya <szeghi.d@gmail.com>
2016-10-08 20:03:02 +0200

    second version with functions

--- hello_world.py
+++ hello_world.py
@@ -2,10 +2,19 @@
 
 import sys
 
-if len(sys.argv) == 1:
-    print('Hello World!')
-else:
-    print("Hello " + sys.argv[1] + "!")
+def print_greetings(length):
+    if length == 0:
+        print('Hello World!')
+    else:
+        print("Hello " + sys.argv[1] + "!")
 
+def arg_length():
+    if len(sys.argv) == 1:
+        return 0
+    else:
+        return 1
+
+x=arg_length()
+print_greetings(x)
 
 
